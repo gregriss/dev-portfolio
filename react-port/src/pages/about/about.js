825e4f10b2a31870ccc14e7be6494e54c9a84fc4
Greg Riss <gregriss23@gmail.com>
2021-05-07 14:52:09 -0400

    add content for About and Contact pages

--- react-port/src/pages/about/about.js
+++ react-port/src/pages/about/about.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 function About() {
     return (
-        <div>About</div>
+        <div id="about-caption">
+            <h1 id="about-h1" class="about">
+                About Me
+            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor" class="bi bi-person-fill icon" viewBox="0 0 16 16">
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                </svg>
+            </h1>
+            <p class="about-me-p lead">
+                I'm a Full Stack Web Developer with an eye for detail and a passion for clarity. I've always been keen at
+                spotting mistakes, even in published books and magazines. My skills include HTML, CSS, JavaScript,
+                jQuery, Node.js, APIs, Express, Handlebars, SQL, and NoSQL.
+          </p>
+        </div>
     )
 }
 
--- react-port/src/pages/contact/contact.js
+++ react-port/src/pages/contact/contact.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 function Contact() {
     return (
-        <div>Contact</div>
+        <div class="container contact-content col-lg-7">
+            <h2 class="contact-h2">
+                Contact
+            <img src="assets/envelope-icon.svg" class="icon" alt="envelope icon" />
+            </h2>
+            <hr />
+            <form netlify>
+                <div class="mb-3">
+                    <label class="form-label">Name</label>
+                    <input name="name" type="text" id="nameInput" class="form-control" placeholder="Name" />
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Email</label>
+                    <input name="email" type="email" id="emailInput" class="form-control" placeholder="Email" />
+                </div>
+                <div class="mb-3">
+                    <label for="userMessage" class="form-label">Message</label>
+                    <textarea id="userMessage" class="form-control" name="userMessage" rows="3"
+                        placeholder="Type your message..."></textarea>
+                </div>
+                <button id="contact-submit-btn" type="submit" class="btn btn-outline-light btn-lg">Submit</button>
+            </form>
+            <hr />
+        </div>
     )
 }
 
