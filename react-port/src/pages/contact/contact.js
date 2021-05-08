6a673bfd227a24b10d64f8539d6ce81ba5aafabb
Greg Riss <gregriss23@gmail.com>
2021-05-07 20:59:00 -0400

    contact page styles

--- react-port/src/pages/contact/contact.js
+++ react-port/src/pages/contact/contact.js
@@ -1,30 +1,30 @@
 import React from 'react';
-
+import './style.css';
 function Contact() {
     return (
-        <div class="container contact-content col-lg-7">
-            <h2 class="contact-h2">
-                Contact
-            <img src="assets/envelope-icon.svg" class="icon" alt="envelope icon" />
-            </h2>
-            <hr />
-            <form netlify>
-                <div class="mb-3">
-                    <label class="form-label">Name</label>
-                    <input name="name" type="text" id="nameInput" class="form-control" placeholder="Name" />
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Email</label>
-                    <input name="email" type="email" id="emailInput" class="form-control" placeholder="Email" />
-                </div>
-                <div class="mb-3">
-                    <label for="userMessage" class="form-label">Message</label>
-                    <textarea id="userMessage" class="form-control" name="userMessage" rows="3"
-                        placeholder="Type your message..."></textarea>
-                </div>
-                <button id="contact-submit-btn" type="submit" class="btn btn-outline-light btn-lg">Submit</button>
-            </form>
-            <hr />
+        <div className="container">
+            <div className="contact-content">
+                <form netlify>
+                    <h2 className="contact-h2">
+                        Contact
+                    <img src="assets/envelope-icon.svg" className="icon" alt="envelope icon" />
+                    </h2>
+                    <div className="form-field">
+                        <label className="form-label">Name</label>
+                        <input name="name" type="text" id="nameInput" className="form-control" placeholder="Name" />
+                    </div>
+                    <div className="form-field">
+                        <label className="form-label">Email</label>
+                        <input name="email" type="email" id="emailInput" className="form-control" placeholder="Email" />
+                    </div>
+                    <div className="form-field">
+                        <label for="userMessage" className="form-label">Message</label>
+                        <textarea id="userMessage" className="form-control" name="userMessage" rows="3"
+                            placeholder="Type your message..."></textarea>
+                    </div>
+                    <button id="contact-submit-btn" type="submit" className="btn btn-outline-light btn-lg">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
